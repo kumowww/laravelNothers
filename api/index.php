454d230f79be1e322f15de6356d7dfec96cfe941
kumowww <wolkerdqq@mail.ru>
2026-04-19 23:46:42 +0200

    fix vercel deploy

--- api/index.php
+++ api/index.php
@@ -1,26 +1,30 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->useStoragePath('/tmp/storage');
 
-$storagePaths = [
-    '/tmp/storage/framework/views',
+$paths = [
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs'
 ];
 
-foreach ($storagePaths as $path) {
+foreach ($paths as $path) {
     if (!is_dir($path)) {
-        @mkdir($path, 0755, true);
+        mkdir($path, 0777, true);
     }
 }
 
+$request = Illuminate\Http\Request::capture();
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+
+$response = $kernel->handle($request);
+
 $response->send();
+
 $kernel->terminate($request, $response);
