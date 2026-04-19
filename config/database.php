909c986857c5c9ab5be51e612bf33e56a682ab70
kumowww <wolkerdqq@mail.ru>
2026-04-19 07:18:31 +0200

    fix

--- config/database.php
+++ config/database.php
@@ -27,4 +27,20 @@ return [
 
     'migrations' => 'migrations',
 
+    'redis' => [
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USER'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6337'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+    ],
+
 ];
