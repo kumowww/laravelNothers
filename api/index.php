cbcabb59583556bf2aa2012d6529b3ec8a5e8b8a
Ernest Kavalski <150811468+kumowww@users.noreply.github.com>
2026-04-19 04:26:17 +0200

    Change storage path and ensure directories exist
    Update storage path and create necessary directories and database file.

--- api/index.php
+++ api/index.php
@@ -4,7 +4,23 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->useStoragePath(storage_path());
+$app->useStoragePath('/tmp');
+
+$storagePaths = [
+    '/tmp/framework/views',
+    '/tmp/framework/cache/data',
+    '/tmp/framework/sessions',
+];
+
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
+}
+
+if (!file_exists('/tmp/database.sqlite')) {
+    touch('/tmp/database.sqlite');
+}
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
