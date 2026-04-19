dafbc814ba8abc00e957ff9835af2f22834e59f8
kumowww <wolkerdqq@mail.ru>
2026-04-19 06:01:05 +0200

    fix

--- app/Http/Middleware/LocaleValidation.php
+++ app/Http/Middleware/LocaleValidation.php
@@ -4,12 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class LocaleValidation
 {
     protected array $validLocales = ['en', 'ru', 'de'];
 
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $locale = $request->route('locale');
 
