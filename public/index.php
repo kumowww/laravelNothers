ed15492ee113b82c8369ec16bcec32dd115ffba1
kumowww <wolkerdqq@mail.ru>
2026-04-20 06:25:49 +0200

    0_0

--- public/index.php
+++ public/index.php
@@ -1,22 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-putenv('VIEW_COMPILED_PATH=/tmp/framework/views');
-
-$paths = [
-    '/tmp/framework/views',
-    '/tmp/framework/cache/data',
-    '/tmp/framework/sessions',
-];
-foreach ($paths as $p) {
-    if (!is_dir($p)) {
-        @mkdir($p, 0755, true);
-    }
-}
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
@@ -24,7 +11,12 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
